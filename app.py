eb7c757111144c1b28cb3d0fdc63be1209cd27eb
bernardino de aguas <bernardino.deaguas@gmail.com>
2023-08-12 14:36:37 -0500

    12 de agosto

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def inicio():
+def index():
     return render_template('sitio/index.html')
 
 @app.route('/DH2O/servicios/')
@@ -18,6 +18,25 @@ def quienes_somos():
 @app.route('/DH2O/mision_vision/')
 def mision_vision():
     return render_template('sitio/mision_vision.html')
+
+@app.route('/DH2O/principios_coorporativos/')
+def principios_coorporativos():
+    return render_template('sitio/principios_coorporativos.html')
+
+@app.route('/DH2O/politica_de_tratamiento_de_datos/')
+def politica_de_tratamiento_de_datos():
+    pdf_filename = 'Política de tratamiento de datos.pdf'
+    return send_file(pdf_filename, as_attachment=True)
+
+@app.route('/contacto')
+def contacto():
+    return render_template('sitio/contacto.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('sitio/productos.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/descuento.js
@@ -0,0 +1,7 @@
+// descuento.js
+
+// Define tu función para calcular el descuento
+function calcularDescuento() {
+  // Aquí puedes implementar la lógica para calcular el descuento en base a tus necesidades
+  return 10; // Ejemplo: un 10% de descuento
+}
